4b91bf50d0c0c8985bea8e59420588e33d994e16
nhathoang <nguyennhathoang35@gmail.com>
2022-04-29 00:14:15 +0700

    fix ui login

--- profile/src/App.js
+++ profile/src/App.js
@@ -2,10 +2,11 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Footer from "./components/Layout/Footer/Footer";
 import Edit from "./components/Edit/Edit";
 import ViewProfile from "./components/ViewProfile/ViewProfile";
 import React, { useEffect, useState } from "react";
-
+import "./components/Layout/Footer/Footer.css"
 function App() {
   const [user, setUser] = useState(null);
 
@@ -19,30 +20,34 @@ function App() {
   }, [user]);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        {!user && (
-          <Route path="/" element={<Login auth={() => setUser(true)} />} />
-        )}
-        {user && (
-          <>
-            <Route
-              path="/view/:id"
-              element={
-                <ViewProfile
-                  logout={() => {
-                    setUser(false);
-                    localStorage.clear();
-                  }}
+    <>
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          {!user && (
+              <Route path="/" element={<Login auth={() => setUser(true)} />} />
+          )}
+          {user && (
+              <>
+                <Route
+                    path="/view/:id"
+                    element={
+                      <ViewProfile
+                          logout={() => {
+                            setUser(false);
+                            localStorage.clear();
+                          }}
+                      />
+                    }
                 />
-              }
-            />
-            <Route path="/edit/:id" element={<Edit auth={user}/>} />
-          </>
-        )}
-      </Routes>
-    </Router>
+                <Route path="/edit/:id" element={<Edit auth={user}/>} />
+              </>
+          )}
+
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
