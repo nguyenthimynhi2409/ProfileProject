8cb0210637e3cc644a73d1a87898bbf94c593e6e
nguyen suri <suringuyen2409@gmail.com>
2022-05-07 16:31:41 +0700

    Updates

--- profile/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./components/TodoList/TodoList.css"
-// import 'bootstrap/dist/css/bootstrap.css';
-
-ReactDOM.render(<App />, document.getElementById("root"));
