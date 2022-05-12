ffe4b70f2c6093649484ad5833f4e55c871d9b2f
nguyen suri <suringuyen2409@gmail.com>
2022-05-12 19:21:13 +0700

    fix user todo

--- profile/src/components/Users/UserTodo.js
+++ profile/src/components/Users/UserTodo.js
@@ -9,65 +9,99 @@ const UserTodo = () => {
 	const {id} = useParams();
 
 	const [todos, setTodos] = useState([]);
-
-	useEffect(() => {
-		getTodos();
-	}, []);
-
+ 	const [todoEdit, setTodoEdit] = useState({});
+  	const [opt, setOtp] = useState(1);
+  	useEffect(() => {
+   	 getTodos();
+  	}, []);
 	const getTodos = async () => {
 		const data = await getTodoByIdUser(id);
 		setTodos(data);
 	};
-
 	const handleCheckboxChange = async (id) => {
 		let res;
 		setTodos(
-			todos.map((todo) => {
-				if (todo.id == id) {
-					todo.completed = !todo.completed;
-					res = todo;
-				}
-				return todo;
-			})
+		todos.map((todo) => {
+			if (todo.id == id) {
+			todo.completed = !todo.completed;
+			res = todo;
+			}
+			return todo;
+		})
 		);
-		const dataTodo = {
-			user: res.user,
-			title: res.title,
-			completed: res.completed
-		}
-		await updateTodo(id, dataTodo);
-	};
+    const dataTodo = {
+      user: res.user,
+      title: res.title,
+      completed: res.completed,
+    };
+    await updateTodo(id, dataTodo);
+  };
 
-	const deleteTodos = async (id) => {
-		setTodos(todos.filter((todo) => todo.id !== id));
-		await deleteTodo(id);
-	};
+  // Edit todo
+  const editTodo = async (id, title) => {
 
-	const addTodo = async (title) => {
-		const todoData = {
-			user: id,
-			title: title,
-			completed: false,
-		};
-		const res = await postTodo(todoData);
-		setTodos([...todos, {...res}]);
-	};
+    let res;
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id == id) {
+          todo.title = title;
+          res = todo;
+        }
+        return todo;
+      })
+    );
+    const dataTodo = {
+      user: res.user,
+      title: res.title,
+      completed: res.completed,
+    };
 
-	return (
-		<div className="todo-container">
-			<TodoHeader/>
-			<AddTodo addTodo={addTodo}/>
-			{todos &&
-				todos.map((todo) => (
-					<TodoItem
-						key={todo.id}
-						todo={todo}
-						handleChange={handleCheckboxChange}
-						deleteTodo={deleteTodos}
-					/>
-				))}
-		</div>
-	);
+    await updateTodo(id, dataTodo);
+  };
+  const getTitle = (id, title) => {
+    setTodoEdit({
+      id: id,
+      todoEdit: title,
+    });
+  };
+
+  const deleteTodos = async (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+    await deleteTodo(id);
+  };
+
+  const addTodo = async (title) => {
+    const todoData = {
+      user: id,
+      title: title,
+      completed: false,
+    };
+    const res = await postTodo(todoData);
+    setTodos([...todos, { ...res }]);
+  };
+
+  return (
+    <div className="todo-container">
+      <TodoHeader />
+
+      <AddTodo
+        editTodo={todoEdit}
+        addTodo={addTodo}
+        updateTodo={editTodo}
+        option={opt}
+        setOption={(opt) => setOtp(opt)}
+      />
+
+
+      <TodoItem
+        datasource={todos}
+        getTodoTitle={getTitle}
+        handleChange={handleCheckboxChange}
+        deleteTodo={deleteTodos}
+        option={(opt) => setOtp(opt)}
+      />
+    </div>
+  );
 };
 
 export default UserTodo;
