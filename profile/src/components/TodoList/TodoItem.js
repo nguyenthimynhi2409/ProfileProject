ce577cb0e6b992caf32ec40aa6cef48db9065c38
nguyen suri <suringuyen2409@gmail.com>
2022-05-12 19:15:23 +0700

    update ui todolist

--- profile/src/components/TodoList/TodoItem.js
+++ profile/src/components/TodoList/TodoItem.js
@@ -1,26 +1,29 @@
-import { Table, Button, Space, Popconfirm } from "antd";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Table, Space, Button, Popconfirm } from "antd";
 
 const TodoItem = (props) => {
-  const [selectedRowsKey, setSelectedRowsKey] = useState([]);
+  // const { completed, id, title } = props.todo;
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
   const columns = [
     {
       width: "50%",
       ellipsis: true,
-      title: "Title",
+      title: "Todo",
       dataIndex: "title",
-      key: "title",
     },
     {
       title: "Action",
       dataIndex: "",
       key: "x",
+      align: "center",
       render: (record) => (
         <Space className="action">
           <Button
             type="primary"
             onClick={() => {
-              props.getTodoTitle(record.id, record.title);
+              console.log(record.key);
+              props.getTodoTitle(record.key, record.title);
               props.option(2);
             }}
           >
@@ -30,7 +33,7 @@ const TodoItem = (props) => {
             title="Are you sure?"
             okText="Yes"
             cancelText="No"
-            onConfirm={() => props.deleteTodo(record.id)}
+            onConfirm={() => props.deleteTodo(record.key)}
           >
             <Button type="danger">
               <a>Delete</a>
@@ -40,20 +43,72 @@ const TodoItem = (props) => {
       ),
     },
   ];
-  const rowSelection = {selectedRowsKey,
-    onSelect: (record, selected, selectedRows) => {
-      console.log(record, selected, selectedRows);
-    }
-  };
 
+  const data = [];
+  if (props.datasource) {
+    for (let i = 0; i < props.datasource.length; i++) {
+      data.push({
+        key: props.datasource[i].id,
+        title: props.datasource[i].title,
+        completed: props.datasource[i].completed,
+        user: props.datasource[i].user,
+      });
+    }
+  }
+
+  useEffect(() => {
+    var arr = [];
+    props.datasource.map((item) => {
+      if (item.completed) arr.push(item.id);
+    });
+    setSelectedRowKeys(arr);
+  }, [props.datasource]);
+  let index;
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: (record) => {
+      if (record.length >= 0) {
+        if (record.length < selectedRowKeys.length) {
+          selectedRowKeys.map((i) => {
+            if (!record.includes(i)) index = i;
+          });
+          props.handleChange(index);
+        } else props.handleChange(record[record.length - 1]);
+      }
+      setSelectedRowKeys(record);
+    },
+  };
   return (
-    <div>
-      <Table
-        rowSelection={{...rowSelection }}
-        columns={columns}
-        dataSource={props.datasource}
-      />
-    </div>
+    // <li className="todo-item">
+    //   <input
+    //     type="checkbox"
+    //     checked={completed}
+    //     onChange={() => props.handleChange(id)}
+    //   />
+    //   <span className={completed ? "completed" : null}>{title}</span>
+    //   <div className="btn_edit">
+    //     <AiFillEdit onClick={() => {
+    //       props.getTodoTitle(id, title);
+    //       props.option(2);
+    //     }
+    //     }/>
+    //   </div>
+    //   <Popconfirm
+    //     title="Are you sure"
+    //     okText="Yes"
+    //     cancelText="No"
+    //     onConfirm={() => props.deleteTodo(id)}
+    //   >
+    //     <button className="btn-style">X</button>
+    //   </Popconfirm>
+    // </li>
+
+    <Table
+      rowSelection={rowSelection}
+      columns={columns}
+      dataSource={data}
+      pagination={false}
+    />
   );
 };
 
